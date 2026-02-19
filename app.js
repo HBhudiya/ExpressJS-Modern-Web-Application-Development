8aef4b6d67a2acc8b8478f61e7f828476ed389a6
HBhudiya <h.bhudiya@outlook.com>
2026-02-19 15:58:29 +0000

    Activity 05 - Implementing All Entity Endpoints

--- app.js
+++ app.js
@@ -59,8 +59,116 @@ const modulesController = async (req, res, variant) => {
   }
 };
 
+const usersController = async (req, res, variant) => {
+  // Initialisations
+  let table = "Users";
+  let fields = [
+    "UserID",
+    "UserFirstname",
+    "UserLastname",
+    "UserEmail",
+    "UserRegistered",
+    "UserLevel",
+    "UserYearID",
+    "UserUsertypeID",
+    "UserImageURL",
+  ];
+  const STAFF = 1;
+
+  // Resolve foreign keys
+  table = `(${table} LEFT JOIN Usertypes ON UserUsertypeID = UsertypeID)`;
+  fields = [...fields, "UsertypeName AS UserusertypeName"];
+  table = `(${table} LEFT JOIN Years ON UserYearID = YearID)`;
+  fields = [...fields, "YearName AS UserYearName"];
+
+  // Build and execute query
+  let where = "";
+  const id = parseInt(req.params.id);
+  switch (variant) {
+    case "primary":
+      where = `WHERE UserID=${id}`;
+      break;
+    case "staff":
+      where = `WHERE UserUsertypeID=${STAFF}`;
+      break;
+    case "groups":
+      table = `(${table} INNER JOIN Groupmembers ON UserID = GroupmemberUserID)`;
+      where = `WHERE GroupmemberGroupID=${id}`;
+      break;
+  }
+
+  const sql = `SELECT ${fields} FROM ${table} ${where}`;
+  try {
+    const [result] = await database.query(sql);
+    if (result.length === 0)
+      res.status(404).json({ message: "No Record(s) Found" });
+    else res.status(200).json(result);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Failed To Execute Query: ${error.message}` });
+  }
+};
+
+const usertypesController = async (req, res, variant) => {
+  // Initialisations
+  let table = "Usertypes";
+  let fields = ["UsertypeID", "UsertypeName"];
+
+  // Resolve foreign keys
+
+  // Build and execute query
+  let where = "";
+  const id = parseInt(req.params.id);
+  switch (variant) {
+    case "primary":
+      where = `WHERE UsertypeID=${id}`;
+      break;
+  }
+
+  const sql = `SELECT ${fields} FROM ${table} ${where}`;
+  try {
+    const [result] = await database.query(sql);
+    if (result.length === 0)
+      res.status(404).json({ message: "No Record(s) Found" });
+    else res.status(200).json(result);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Failed To Execute Query: ${error.message}` });
+  }
+};
+
+const yearsController = async (req, res, variant) => {
+  // Initialisations
+  let table = "years";
+  let fields = ["YearID", "YearName"];
+
+  // Resolve foreign keys
+  // Build and execute query
+  let where = "";
+  const id = parseInt(req.params.id);
+  switch (variant) {
+    case "primary":
+      where = `WHERE YearID=${id}`;
+      break;
+  }
+
+  const sql = `SELECT ${fields} FROM ${table} ${where}`;
+  try {
+    const [result] = await database.query(sql);
+    if (result.length === 0)
+      res.status(404).json({ message: "No Record(s) Found" });
+    else res.status(200).json(result);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Failed To Execute Query: ${error.message}` });
+  }
+};
+
 // Endpoints
-app.get("/api/modules", modulesController);
+app.get("/api/modules", (req, res) => modulesController(req, res, null));
 app.get("/api/modules/:id", (req, res) =>
   modulesController(req, res, "primary"),
 );
@@ -70,6 +178,21 @@ app.get("/api/modules/leader/:id", (req, res) =>
 app.get("/api/modules/users/:id", (req, res) =>
   modulesController(req, res, "users"),
 );
+
+app.get("/api/users", (req, res) => usersController(req, res, null));
+app.get("/api/users/staff", (req, res) => usersController(req, res, "staff"));
+app.get("/api/users/:id", (req, res) => usersController(req, res, "primary"));
+app.get("/api/users/groups/:id", (req, res) =>
+  usersController(req, res, "groups"),
+);
+
+app.get("/api/usertypes", (req, res) => usertypesController(req, res, null));
+app.get("/api/usertypes/:id", (req, res) =>
+  usertypesController(req, res, "primary"),
+);
+
+app.get("/api/years", (req, res) => yearsController(req, res, null));
+app.get("/api/years/:id", (req, res) => yearsController(req, res, "primary"));
 
 // Start server
 const PORT = process.env.PORT || 5000;
